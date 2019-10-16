fee38474ff1a3865f49a878d9c76f81b7a688463
Tis Tiller <38237168+TisTiller@users.noreply.github.com>
2019-10-16 20:07:46 +1000

    Initial Commit

---
+++ brightness.sh
@@ -0,0 +1,11 @@
+maxb="$(sudo cat /sys/class/backlight/intel_backlight/max_brightness)"
+echo "Max Brightness: $maxb"
+printf "Brightness: "
+
+read brightness
+if [ $brightness -gt $maxb ]
+then
+	echo "Too Big."
+else
+	echo $brightness | sudo tee -a /sys/class/backlight/intel_backlight/brightness > /dev/null
+fi
